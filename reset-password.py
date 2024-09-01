14931a699a21765d673186e5459e46d1a1e175a3
ayushi.gupta <gupta.ayushi2016@gmail.com>
2024-09-01 20:58:40 +0530

    added reset password

---
+++ reset-password.py
@@ -0,0 +1,7 @@
+username = int(input("enter Username="))
+password = int(input("enter Password="))
+
+if username == "Admin" and password == "Admin":
+    print("you can reste")
+else:
+    print("You can not reset")
